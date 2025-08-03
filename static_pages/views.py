483750a51a0be5df328c7ef0f59d33d5b197ce37
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2025-08-03 12:07:08 +0200

    Install black and configure it for the project
    Add the black code formatter to the project dependencies and configure
    it with a line length of 120 characters, targeting Python 3.13.
    The configuration also includes Python files with the `.py` and `.pyi`
    extensions.
    
    Run black on the existing codebase to format it according to the new
    settings.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- static_pages/views.py
+++ static_pages/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 class HomeView(TemplateView):
     template_name = "static_pages/home.html"
+
 
 # Create your views here.
