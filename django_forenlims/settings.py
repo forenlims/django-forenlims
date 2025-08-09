bb3df9b974c64a64fb82e9322e39fb4a25d2f21a
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2025-08-09 15:19:17 +0200

    Install django-extensions
    Install the django-extensions package to provide enhanced management
    commands.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- django_forenlims/settings.py
+++ django_forenlims/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "debug_toolbar",
+    "django_extensions",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
