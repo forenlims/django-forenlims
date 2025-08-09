f5e338625ce1a227cc37fedf17f1e720a0a2996e
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2025-08-09 12:43:17 +0200

    Add Custom User Model to the accounts app
    Implement a custom user model extending Django's AbstractUser.
    Add a first name and last name field to the custom user model.
    Include the custom user model in Django settings and Admin.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
-# from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=20, blank=True)
+    last_name = models.CharField(max_length=20, blank=True)
